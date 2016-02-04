a7c1811a1f94c3bb8bbe850189c4c1178e7a56d1
Todd Stout <todd.tstout@gmail.com>
2016-02-04 07:12:22 -0600

    bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.tstout/fin-kratzen "0.1.0"
+(defproject com.github.tstout/fin-kratzen "0.1.1"
             :description "Financial Scraper"
             :url "https://github.com/tstout/fin-kratzen"
             :license {:name "MIT"
