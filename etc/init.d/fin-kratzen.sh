93e24e78b0b1f3685e30b0dfc45469c55670b3ba
Todd Stout <todd.tstout@gmail.com>
2016-02-21 18:03:19 -0600

    bump version

--- etc/init.d/fin-kratzen.sh
+++ etc/init.d/fin-kratzen.sh
@@ -14,7 +14,7 @@
 # sudo /usr/sbin/update-rc.d -f fin-kratzen.sh defaults
 
 WORK_DIR="/home/pi/fin-kratzen"
-JAR="fin-kratzen-0.1.1-standalone.jar"
+JAR="fin-kratzen-0.1.2-standalone.jar"
 USER="pi"
 DAEMON="/usr/bin/java"
 DAEMON_ARGS="-server \
