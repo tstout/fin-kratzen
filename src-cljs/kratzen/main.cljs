6f9b1f9c0d3943541e6eee4726221408f8732515
Todd Stout <todd.tstout@gmail.com>
2017-12-17 09:46:08 -0600

    remove om dep

--- src-cljs/kratzen/main.cljs
+++ src-cljs/kratzen/main.cljs
@@ -1,12 +1,6 @@
 (ns ^:figwheel-always kratzen.main
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [om-bootstrap.table :refer [table]]
-            [om-bootstrap.grid :as g]
-            [om-bootstrap.button :as b]
-            [om-tools.dom :as d :include-macros true]
-            [cljs.core.async :refer [put! chan <! pub]]
+  (:require [cljs.core.async :refer [put! chan <! pub]]
             [clojure.data :as data]
             [clojure.string :as string]))
 ;;
@@ -26,28 +20,3 @@
                             :posting_date   #inst "2015-06-26"
                             :bank_id        "00095060626-66.7301506268453.87"}
                            ]}))
-
-(defn stmt-view [{:keys [posting_date description amount]} owner]
-  (reify
-    om/IRender
-    (render [this]
-      (d/tr
-        (d/td (str posting_date))
-        (d/td description)
-        (d/td amount)))))
-
-(defn stmts-view [data owner]
-  (reify
-    om/IRender
-    (render [this]
-      (table {:striped? true :bordered? true :condensed? true :hover? true}
-             (d/thead
-               (d/tr
-                 (d/th "Posting Date")
-                 (d/th "Description")
-                 (d/th "Amount")))
-             (d/tbody
-               (om/build-all stmt-view (:boa-stmts data)))))))
-
-(om/root stmts-view app-state
-         {:target (. js/document (getElementById "app"))})
